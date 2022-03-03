9fdb497a9ba6e1f17fbbd6c8a37cf0707b91ac86
Jonatan Hamlet Munguia Martinez <hunterhamlet@gmail.com>
2022-03-02 21:28:44 -0600

    feat: Add button styles and label style

--- bankodemia/Dimensions.swift
+++ bankodemia/Dimensions.swift
@@ -8,11 +8,20 @@
 import Foundation
 
 class Dimensions {
+    
+    //MARK: Dimensions for margins
+    static let grid0_5 = 2.0
     static let grid1 = 4.0
+    static let grid1_5 = 6.0
     static let grid2 = 8.0
     static let grid3 = 12.0
     static let grid4 = 16.0
     static let grid5 = 20.0
     static let grid6 = 24.0
     static let grid7 = 28.0
+    static let grid8 = 32.0
+    
+    //MARK: Specific dimensions
+    static let tilHeight = 70.0
+    static let btHeight = 42.0
 }
---
+++ bankodemia/UIBUttonExtensions.swift
@@ -0,0 +1,28 @@
+//
+//  UIBUttonExtensions.swift
+//  bankodemia
+//
+//  Created by Hamon on 02/03/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    
+    func applySolidStyle(fontSize : CGFloat = FontSize.defaulSize) {
+        self.backgroundColor = Theme.Color.blue
+        self.setTitleColor(.white, for: .normal)
+        self.layer.cornerRadius = Dimensions.grid1_5
+        self.titleLabel?.font = UIFont(name: Theme.Font.popinsMedium, size: fontSize)
+    }
+    
+    func applyOutlinedStyle(fontSize : CGFloat = FontSize.defaulSize) {
+        self.setTitleColor(Theme.Color.blue, for: .normal)
+        self.layer.borderWidth = Dimensions.grid0_5
+        self.layer.borderColor = Theme.Color.blue.cgColor
+        self.titleLabel?.font = UIFont(name: Theme.Font.popinsMedium, size: fontSize)
+        self.layer.cornerRadius = Dimensions.grid1_5
+    }
+    
+}

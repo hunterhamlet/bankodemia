c1270f02a7c9cde040e07c52e35d0f8d4e7c1591
Markeif <marco.cruz.velazquez@gmail.com>
2022-03-28 00:32:36 -0600

    feat:CS-6 Clabe and Card ViewControllers were created and are linked

--- bankodemia/Theme.swift
+++ bankodemia/Theme.swift
@@ -13,6 +13,7 @@ struct Theme {
     struct Color {
         static let background = UIColor.white
         static let blue = UIColor(red: 0, green: 0.8, blue: 0.8, alpha: 0.9)
+        static let black = UIColor.black
     }
     
     struct Font {
